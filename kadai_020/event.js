4293f2d6e8b9ce9f0fc4009033a2f0d523578015
NTOMONO <14ryo.junetomo@gmail.com>
2025-02-06 14:28:46 +0900

    課題提出

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    const childList = document.createElement('text');
+    childList.textContent = 'ボタンをクリックしました';
+
+    parentList.removeChild(parentList.firstChild);
+    parentList.appendChild(childList);
+});
